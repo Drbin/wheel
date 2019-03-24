27e5fa57c6987c311c1f25f6c05cde2d70bbdce6
dairubin <314492880@qq.com>
2019-03-24 17:21:34 +0800

    第七天

---
+++ ts/typeArr.js
@@ -0,0 +1,12 @@
+var arr;
+var i;
+/*数值数组*/
+arr = [1, 3, 4];
+for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+/*字符数组*/
+arr = ["str1", "str2"];
+for (i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
